9f7c85ba3ffa8dd1897f82c3c309a9768bed2d20
おれん Oren <151939066+too-deve1oper@users.noreply.github.com>
2024-02-09 22:24:41 +0900

    Fixed some errors

--- source/backend/CoolUtil.hx
+++ source/backend/CoolUtil.hx
@@ -170,4 +170,11 @@ class CoolUtil
 			Lib.application.window.y + (Lib.application.window.height / 2)
 		);
 	}
+	
+	public static function boundTo(value:Float, min:Float, max:Float):Float {
+		var newValue:Float = value;
+		if(newValue < min) newValue = min;
+		else if(newValue > max) newValue = max;
+		return newValue;
+	}
 }
